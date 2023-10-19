8c468d895ccca4d5090201e8e194adbfc2a70632
fr33zingH3ll <samuelnicod66@gmail.com>
2023-10-19 08:35:19 +0200

    fix: app.sh pour windows fonctionne(a peu pres)

--- app.sh
+++ app.sh
@@ -2,7 +2,6 @@
 
 # Variables
 os_info=$(uname)
-echo $os_info
 venv="tp_pass_force_venv"
 module_name="index"
 destination="html/tp_pass_force"
@@ -16,18 +15,13 @@ elif [ "$os_info" == "Darwin" ]; then
     source "$venv"/bin/activate
 elif [ "$os_info" == "MINGW64_NT-10.0-19045" ]; then  # Vérifiez la chaîne de la fenêtre Git Bash pour Windows
     echo "Système d'exploitation : Windows"
-    source ".\tp_pass_force_venv\Scripts\activate"
+    source "./$venv\Scripts\activate"
 else
     echo "Système d'exploitation non reconnu : $os_info"
 fi
 
-# Activez l'environnement virtuel
-if [ -d "$venv" ]; then
-    source "$venv"/bin/activate
-else
-    echo "L'environnement virtuel $venv n'a pas été trouvé. Assurez-vous qu'il existe dans le répertoire actuel."
-    exit 1
-fi
+# Mettre a jour pip
+pip install --upgrade pip
 
 # Installez les dépendances à partir du fichier requirements.txt
 pip install -r requirements.txt
@@ -40,7 +34,7 @@ if [ "$os_info" == "Linux" ]; then
     xdg-open "$destination/$module_name.html"  # Pour les systèmes Linux
 elif [ "$os_info" == "Darwin" ]; then
     open "$destination/$module_name.html"  # Pour les systèmes macOS
-elif [ "$os_info" == "MINGW64_NT-10.0" ]; then
+elif [ "$os_info" == "MINGW64_NT-10.0-19045" ]; then
     start "$destination\\$module_name.html"  # Pour les systèmes Windows (Git Bash)
 else
     echo "L'ouverture de la documentation n'est pas prise en charge sur ce système."
